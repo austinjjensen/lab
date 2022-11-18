e210f75ec24d8d562c8c97c7ea0b87ee0e9f7856
austinjjensen <118563273+austinjjensen@users.noreply.github.com>
2022-11-18 13:58:08 -0600

    Added docstrings and type hints to methods.

--- account.py
+++ account.py
@@ -1,22 +1,23 @@
 class Account:
     def __int__(self, name):
+        """Set default values for account objects"""
         self.account_name = name
         self.account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> float:
         if(amount >= 0 ):
             self.account_balance = self.account_balance + amount
         else:
             print('Cannot deposit a negative amount')
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> float:
         if(amount <= self.account_balance):
             self.account_balance = self.account_balance - amount
         else:
             print('Cannot withdraw more than your account balance')
 
-    def get_name(self):
+    def get_name(self: str) -> str:
         print('Name: ', self.account_name)
 
-    def get_balance(self):
+    def get_balance(self: float) -> float:
         print('Account Balance: ', self.account_balance)

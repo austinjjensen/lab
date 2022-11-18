ac1c1b8be6a2ca81fe2a4ef26731cdaf10d75dc1
austinjjensen <118563273+austinjjensen@users.noreply.github.com>
2022-11-18 14:27:16 -0600

    Added unittest file.

--- test_account.py
+++ test_account.py
@@ -1,9 +1,17 @@
 import unittest
 from account import *
 
+
 class MyTestCase(unittest.TestCase):
     def test_init(self):
-        self.assertEqual(self.)
+        self.assertEqual(self.account_name(), 'John Smith')
+        self.assertEqual(self.account_balance(), 500)
+
+    def test_deposit(self):
+        self.assertEqual(deposit(500), 1000)
+
+    def test_withdraw(self):
+        self.assertEqual(withdraw(200), 800)
 
 
 if __name__ == '__main__':

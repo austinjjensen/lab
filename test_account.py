9b93809dc9a22f05b4332c827a58b6a9d9803b83
austinjjensen <118563273+austinjjensen@users.noreply.github.com>
2022-11-18 14:07:01 -0600

    Added docstrings and type hints to methods.

---
+++ test_account.py
@@ -0,0 +1,10 @@
+import unittest
+from account import *
+
+class MyTestCase(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual(self.)
+
+
+if __name__ == '__main__':
+    unittest.main()
